86975da0a6a0a14ae939a21083c31da629110016
mdarmankhan6252 <mdarmankhan6252@gmail.com>
2024-12-24 04:23:23 +0600

    I have added live link

--- src/carousels/Carousel.jsx
+++ src/carousels/Carousel.jsx
@@ -11,28 +11,28 @@ const Carousel = () => {
          {/* carousel - 1 */}
          <div className=" font-serif flex items-end space-x-4">
             <h1 className="text-5xl">Carousal - 1 : </h1>
-            <Link className="text-blue-500 hover:underline text-2xl">Get Code</Link>
+            <Link target="_blank" to='https://github.com/mdarmankhan6252/custom-components/blob/main/src/carousels/Carousel.jsx' className="text-blue-500 hover:underline text-2xl">Get Code</Link>
          </div>
          <Carousel1 />
 
          {/* carousel - 2 */}
          <div className=" font-serif flex items-end space-x-4 pb-6">
             <h1 className="text-5xl">Carousal - 2 : </h1>
-            <Link className="text-blue-500 hover:underline text-2xl">Get Code</Link>
+            <Link target="_blank" to='https://github.com/mdarmankhan6252/custom-components/blob/main/src/carousels/Carousel2.jsx' className="text-blue-500 hover:underline text-2xl">Get Code</Link>
          </div>
          <Carousel2 />
 
          {/* carousel - 3 */}
          <div className=" font-serif flex items-end space-x-4 pt-20 pb-6">
             <h1 className="text-5xl">Carousal - 3 : </h1>
-            <Link className="text-blue-500 hover:underline text-2xl">Get Code</Link>
+            <Link target="_blank" to='https://github.com/mdarmankhan6252/custom-components/blob/main/src/carousels/Carousel3.jsx' className="text-blue-500 hover:underline text-2xl">Get Code</Link>
          </div>
          <Carousel3 />
 
          {/* carousel - 4 */}
          <div className=" font-serif flex items-end space-x-4 pt-20 pb-6">
             <h1 className="text-5xl">Carousal - 4 : </h1>
-            <Link className="text-blue-500 hover:underline text-2xl">Get Code</Link>
+            <Link target="_blank" to='https://github.com/mdarmankhan6252/custom-components/blob/main/src/carousels/Carousel4.jsx' className="text-blue-500 hover:underline text-2xl">Get Code</Link>
          </div>
          <Carousel4 />
 
